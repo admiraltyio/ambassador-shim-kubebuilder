230f8f1ba5022c410c6f0d460e2484696a04a607
Adrien Trouillaud <adrien@Adriens-MacBook-Air.local>
2018-08-26 12:06:24 -0700

    kubebuilder create api --group ambassadorshim --version v1alpha1 --kind Mapping

---
+++ pkg/apis/ambassadorshim/v1alpha1/mapping_types.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2018 Admiralty Technologies Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// MappingSpec defines the desired state of Mapping
+type MappingSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// MappingStatus defines the observed state of Mapping
+type MappingStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Mapping is the Schema for the mappings API
+// +k8s:openapi-gen=true
+type Mapping struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MappingSpec   `json:"spec,omitempty"`
+	Status MappingStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// MappingList contains a list of Mapping
+type MappingList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Mapping `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Mapping{}, &MappingList{})
+}
